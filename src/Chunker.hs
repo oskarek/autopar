c80bc6c96e539dd78bb82556f7846bdc55a739ef
Oskar Ek <oskarek24@gmail.com>
2020-03-25 22:14:42 +0100

    Simplifications for Chunker

--- src/Chunker.hs
+++ src/Chunker.hs
@@ -1,8 +1,11 @@
 module Chunker (
     Chunker
   , (>->)
+  , (<-<)
+  , cconcat
   , evalChunker
   , chunkOf
+  , empty
   , one
   , chunk
   , repeatedly
@@ -14,49 +17,62 @@ import           Control.Applicative            ( liftA2 )
 import           Control.DeepSeq                ( NFData )
 import           Control.Monad.State
 import           Control.Monad.Reader
+import           Data.Bool                      ( bool )
 import           System.IO.Unsafe               ( unsafePerformIO )
 import           Util
+
+-- TODO: Maybe make `Chunker a` a Monoid
 
 type Chunker a = StateT (ChunkerState a) (Reader PicoSeconds) [[a]]
 data ChunkerState a = ChunkerState { chunkSize :: Int, list :: [a] }
 
 infixr 1 >->
+infixr 1 <-<
 
--- | Compose two chunkers.
+-- | Compose two chunkers, left to right.
 (>->) :: Chunker a -> Chunker a -> Chunker a
-(>->) = liftA2 (++)
+ch1 >-> ch2 = liftA2 (++) ch1 ch2Checked
+  where ch2Checked = null <$> gets list >>= bool ch2 empty
+
+-- | Compose two chunkers, right to left.
+(<-<) :: Chunker a -> Chunker a -> Chunker a
+(<-<) = flip (>->)
+
+-- | Concat multiple chunkers, to run sequentially.
+cconcat :: Foldable f => f (Chunker a) -> Chunker a
+cconcat = foldr (>->) empty
 
 -- | Evaluate a Chunker and get the final value.
 evalChunker :: Chunker a -> PicoSeconds -> [a] -> [[a]]
 evalChunker ch optTime =
-  fst . flip runReader optTime . runStateT ch . ChunkerState 8
+  flip runReader optTime . evalStateT ch . ChunkerState 8
 
--- | Create a Chunker that extracts a chunk of the given size.
+-- | Chunker that extracts a chunk of the given size.
 chunkOf :: Int -> Chunker a
 chunkOf n = do
   (chnk, rest) <- splitAt n <$> gets list
   modify (\(ChunkerState size _) -> ChunkerState size rest)
   return [chnk]
 
--- | Creates a chunk of the first element.
+-- | Chunker that does nothing. The identity for chunker composition.
+empty :: Chunker a
+empty = return []
+
+-- | Chunker that extracts a chunk of size 1.
 one :: Chunker a
 one = chunkOf 1
 
--- | Creates a chunk, the size of the current value of chunkSize.
+-- | Chunker that creates a chunk, the size of the current value of chunkSize.
 chunk :: Chunker a
 chunk = gets chunkSize >>= chunkOf
 
 -- | Chunks the remainder of the list, using the given Chunker repeatedly.
 repeatedly :: Chunker a -> Chunker a
-repeatedly chunker = do
-  done <- null <$> gets list
-  if done then pure [] else chunker >-> repeatedly chunker
+repeatedly = cconcat . repeat
 
 -- | Apply a Chunker up to n times.
 count :: Int -> Chunker a -> Chunker a
-count n chunker = do
-  done <- null <$> gets list
-  if n <= 0 || done then pure [] else chunker >-> count (n - 1) chunker
+count n = cconcat . replicate n
 
 -- | Applies the given Chunker, measures how long it takes to
 -- evaluate the contained elements to normal form, and updates
